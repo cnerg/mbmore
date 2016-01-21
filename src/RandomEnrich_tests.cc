90b9e8f496b895c81df63bb11f33b202def18ecf
Meghan McGarry <mcgarry.fc@gmail.com>
2016-01-20 18:10:44 -0600

    got skeleton tests working for Enrichment. Just contains original EF mocksim tests so far

--- src/RandomEnrich_tests.cc
+++ src/RandomEnrich_tests.cc
@@ -1,15 +1,6 @@
 #include <gtest/gtest.h>
 
-#include <sstream>
-
-#include "facility_tests.h"
-#include "toolkit/mat_query.h"
-#include "agent_tests.h"
-#include "resource_helpers.h"
-#include "infile_tree.h"
-#include "env.h"
-
-#include "enrichment_tests.h"
+#include "cyclus.h"
 
 using cyclus::QueryResult;
 using cyclus::Cond;
@@ -17,9 +8,12 @@ using cyclus::CompMap;
 using cyclus::toolkit::MatQuery;
 using pyne::nucname::id;
 using cyclus::Material;
+using cyclus::Composition;
 
-namespace cycamore {
+namespace mbmore {
   
+namespace randomenrichtests {
+
 Composition::Ptr c_nou235() {
   cyclus::CompMap m;
   m[922380000] = 1.0;
@@ -50,8 +44,8 @@ Composition::Ptr c_heu() {
   return Composition::CreateFromMass(m);
 };
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, RequestQty) {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(RandomEnrichTests, RequestQty) {
   // this tests verifies that requests for input material are fulfilled
   // without providing any extra
   
@@ -65,7 +59,7 @@ TEST_F(EnrichmentTest, RequestQty) {
 
   int simdur = 1;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   
   sim.AddSource("natu")
@@ -87,7 +81,7 @@ TEST_F(EnrichmentTest, RequestQty) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, CheckSWUConstraint) {
+TEST(RandomEnrichTests, CheckSWUConstraint) {
   // Tests that request for enrichment that exceeds the SWU constraint
   // fulfilled only up to the available SWU.
   // Also confirms that initial_feed flag works.
@@ -105,7 +99,7 @@ TEST_F(EnrichmentTest, CheckSWUConstraint) {
   int simdur = 1;
   
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
    
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("heu", c_heu());
@@ -128,7 +122,7 @@ TEST_F(EnrichmentTest, CheckSWUConstraint) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, CheckCapConstraint) {
+TEST(RandomEnrichTests, CheckCapConstraint) {
   // Tests that a request for more material than is available in
   // inventory is partially filled with only the inventory quantity.
 
@@ -143,7 +137,7 @@ TEST_F(EnrichmentTest, CheckCapConstraint) {
   int simdur = 1;
 
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
 
 
   sim.AddRecipe("natu1", c_natu1());
@@ -167,7 +161,7 @@ TEST_F(EnrichmentTest, CheckCapConstraint) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, RequestEnrich) {
+TEST(RandomEnrichTests, RequestEnrich) {
   // this tests verifies that requests for output material exceeding
   // the maximum allowed enrichment are not fulfilled.
   
@@ -181,7 +175,7 @@ TEST_F(EnrichmentTest, RequestEnrich) {
 
   int simdur = 2;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("leu", c_leu());
   sim.AddRecipe("heu", c_heu());
@@ -223,7 +217,7 @@ TEST_F(EnrichmentTest, RequestEnrich) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, TradeTails) {
+TEST(RandomEnrichTests, TradeTails) {
   // this tests whether tails are being traded.
 
   std::string config = 
@@ -236,7 +230,7 @@ TEST_F(EnrichmentTest, TradeTails) {
   // time 1-source to EF, 2-Enrich, add to tails, 3-tails avail. for trade
   int simdur = 3;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("leu", c_leu());
   
@@ -260,7 +254,7 @@ TEST_F(EnrichmentTest, TradeTails) {
   
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  TEST_F(EnrichmentTest, TailsQty) {
+  TEST(RandomEnrichTests, TailsQty) {
   // this tests whether tails are being traded at correct quantity when
   // requested amount is larger than qty in a single tails-buffer element
 
@@ -274,7 +268,7 @@ TEST_F(EnrichmentTest, TradeTails) {
   // time 1-source to EF, 2-Enrich, add to tails, 3-tails avail. for trade
   int simdur = 3;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("leu", c_leu());
   
@@ -315,7 +309,7 @@ TEST_F(EnrichmentTest, TradeTails) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, BidPrefs) {
+TEST(RandomEnrichTests, BidPrefs) {
   // This tests that natu sources are preference-ordered by
   // U235 content
 
@@ -329,7 +323,7 @@ TEST_F(EnrichmentTest, BidPrefs) {
 
   int simdur = 1;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("natu2", c_natu2());
 
@@ -366,7 +360,7 @@ TEST_F(EnrichmentTest, BidPrefs) {
   
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  TEST_F(EnrichmentTest, NoBidPrefs) {
+  TEST(RandomEnrichTests, NoBidPrefs) {
   // This tests that preference-ordering for sources
   // turns off correctly if flag is used
 
@@ -381,7 +375,7 @@ TEST_F(EnrichmentTest, BidPrefs) {
 
   int simdur = 1;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("natu1", c_natu1());
   sim.AddRecipe("natu2", c_natu2());
 
@@ -406,7 +400,7 @@ TEST_F(EnrichmentTest, BidPrefs) {
   }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, ZeroU235) {
+TEST(RandomEnrichTests, ZeroU235) {
   // Test that offers of natu with no u235 content are rejected
 
   std::string config = 
@@ -419,7 +413,7 @@ TEST_F(EnrichmentTest, ZeroU235) {
 
   int simdur = 1;
   cyclus::MockSim sim(cyclus::AgentSpec
-		      (":cycamore:Enrichment"), config, simdur);
+		      (":mbmore:RandomEnrich"), config, simdur);
   sim.AddRecipe("no_u235", c_nou235());
   sim.AddRecipe("natu1", c_natu1());
 
@@ -438,307 +432,7 @@ TEST_F(EnrichmentTest, ZeroU235) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void EnrichmentTest::SetUp() {
-  cyclus::Env::SetNucDataPath();
-  cyclus::Context* ctx = tc_.get();
-  src_facility = new Enrichment(ctx);
-  trader = tc_.trader();
-  InitParameters();
-  SetUpSource();
-}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void EnrichmentTest::TearDown() {
-  delete src_facility;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void EnrichmentTest::InitParameters() {
-  cyclus::Context* ctx = tc_.get();
-
-  feed_commod = "incommod";
-  product_commod = "outcommod";
-  tails_commod = "tailscommod";
-
-  feed_recipe = "recipe";
-  feed_assay = 0.0072;
-
-  cyclus::CompMap v;
-  v[922350000] = feed_assay;
-  v[922380000] = 1 - feed_assay;
-  recipe = cyclus::Composition::CreateFromAtom(v);
-  ctx->AddRecipe(feed_recipe, recipe);
-
-  tails_assay = 0.002;
-  swu_capacity = 100; //**
-  inv_size = 5;
-
-  reserves = 105.5;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void EnrichmentTest::SetUpSource() {
-  src_facility->feed_recipe = feed_recipe;
-  src_facility->feed_commod = feed_commod;
-  src_facility->product_commod = product_commod;
-  src_facility->tails_commod = tails_commod;
-  src_facility->tails_assay = tails_assay;
-  src_facility->max_enrich = max_enrich;
-  src_facility->SetMaxInventorySize(inv_size);
-  src_facility->SwuCapacity(swu_capacity);
-  src_facility->initial_feed = reserves;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Material::Ptr EnrichmentTest::GetMat(double qty) {
-  return cyclus::Material::CreateUntracked(qty,
-                                           tc_.get()->GetRecipe(feed_recipe));
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void EnrichmentTest::DoAddMat(cyclus::Material::Ptr mat) {
-  src_facility->AddMat_(mat);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Material::Ptr EnrichmentTest::DoRequest() {
-  return src_facility->Request_();
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Material::Ptr
-EnrichmentTest::DoOffer(cyclus::Material::Ptr mat) {
-  return src_facility->Offer_(mat);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Material::Ptr
-EnrichmentTest::DoEnrich(cyclus::Material::Ptr mat, double qty) {
-  return src_facility->Enrich_(mat, qty);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, Request) {
-  // Tests that quantity in material request is accurate
-  double req = inv_size;
-  double add = 0;
-  cyclus::Material::Ptr mat = DoRequest();
-  EXPECT_DOUBLE_EQ(mat->quantity(), req);
-  EXPECT_EQ(mat->comp(), tc_.get()->GetRecipe(feed_recipe));
-
-  add = 2 * inv_size / 3;
-  req -= add;
-  DoAddMat(GetMat(add));
-  mat = DoRequest();
-  EXPECT_DOUBLE_EQ(mat->quantity(), req);
-  EXPECT_EQ(mat->comp(), tc_.get()->GetRecipe(feed_recipe));
-
-  add = inv_size / 3;
-  req = 0;
-  DoAddMat(GetMat(add));
-  mat = DoRequest();
-  EXPECT_DOUBLE_EQ(mat->quantity(), req);
-  EXPECT_EQ(mat->comp(), tc_.get()->GetRecipe(feed_recipe));
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, ValidReq) {
-  // Tests that material requests have U235/(U235+U238) > tails assay
-  using cyclus::CompMap;
-  using cyclus::Composition;
-  using cyclus::Material;
-
-  double qty = 4.5;  // some magic number
-
-  cyclus::CompMap v1;
-  v1[922350000] = 1;
-  Material::Ptr mat = Material::CreateUntracked
-    (qty,Composition::CreateFromAtom(v1));
-  EXPECT_FALSE(src_facility->ValidReq(mat));  // u238 = 0
-
-  cyclus::CompMap v2;
-  v2[922350000] = tails_assay;
-  v2[922380000] = 1 - tails_assay;
-  mat = Material::CreateUntracked(qty, Composition::CreateFromAtom(v2));
-  // u235 / (u235 + u238) <= tails_assay
-  EXPECT_FALSE(src_facility->ValidReq(mat)); 
-
-  cyclus::CompMap v3;
-  v3[922350000] = 1;
-  v3[922380000] = 1;
-  mat = Material::CreateUntracked(qty, Composition::CreateFromAtom(v3));
-  EXPECT_TRUE(src_facility->ValidReq(mat));  // valid
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  TEST_F(EnrichmentTest, ConstraintConverters) {
-    // Tests the SWU and NatU converters to make sure that amount of
-    // feed and SWU required are correct to fulfill the enrichment request.
-  using cyclus::CompMap;
-  using cyclus::Material;
-  using cyclus::toolkit::MatQuery;
-  using cyclus::Composition;
-  using cyclus::toolkit::Assays;
-  using cyclus::toolkit::UraniumAssay;
-  using cyclus::toolkit::SwuRequired;
-  using cyclus::toolkit::FeedQty;
-  using cyclus::toolkit::MatQuery;
-  cyclus::Env::SetNucDataPath();
-
-  double qty = 5;  // 5 kg
-  double product_assay = 0.05;  // of 5 w/o enriched U
-  CompMap v;
-  v[922350000] = product_assay;
-  v[922380000] = 1 - product_assay;
-  v[94239] = 0.5;  // 94239 shouldn't be taken into account
-  Material::Ptr target = Material::CreateUntracked(
-      qty, Composition::CreateFromMass(v));
-
-  std::set<cyclus::Nuc> nucs;
-  nucs.insert(922350000);
-  nucs.insert(922380000);
-
-  MatQuery mq(target);
-  double mass_frac = mq.mass_frac(nucs);
-  
-  SWUConverter swuc(feed_assay, tails_assay);
-  NatUConverter natuc(feed_assay, tails_assay);
-
-  Material::Ptr offer = DoOffer(target);
-
-  EXPECT_NEAR(swuc.convert(target), swuc.convert(offer), 0.001);
-  EXPECT_NEAR(natuc.convert(target) * mass_frac, natuc.convert(offer), 0.001); 
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, Enrich) {
-  // this test asks the facility to enrich a material that results in an amount
-  // of natural uranium required that is exactly its inventory level. that
-  // inventory will be comprised of two materials to test the manifest/absorb
-  // strategy employed in Enrich_.
-  using cyclus::CompMap;
-  using cyclus::Material;
-  using cyclus::toolkit::MatQuery;
-  using cyclus::Composition;
-  using cyclus::toolkit::Assays;
-  using cyclus::toolkit::UraniumAssay;
-  using cyclus::toolkit::SwuRequired;
-  using cyclus::toolkit::FeedQty;
-
-  double qty = 5;  // kg
-  double product_assay = 0.05;  // of 5 w/o enriched U
-  cyclus::CompMap v;
-  v[922350000] = product_assay;
-  v[922380000] = 1 - product_assay;
-  // target qty need not be = to request qty
-  Material::Ptr target = cyclus::Material::CreateUntracked(
-      qty + 10, cyclus::Composition::CreateFromMass(v));
-
-  Assays assays(feed_assay, UraniumAssay(target), tails_assay);
-  double swu_req = SwuRequired(qty, assays);
-  double natu_req = FeedQty(qty, assays);
-  double tails_qty = TailsQty(qty, assays);
-
-  double swu_cap = swu_req * 5;
-  src_facility->SwuCapacity(swu_cap);
-  src_facility->SetMaxInventorySize(natu_req);
-  DoAddMat(GetMat(natu_req / 2));
-  DoAddMat(GetMat(natu_req / 2));
-
-  Material::Ptr response;
-  EXPECT_NO_THROW(response = DoEnrich(target, qty));
-  EXPECT_DOUBLE_EQ(src_facility->Tails().quantity(), tails_qty); 
-  
-  MatQuery q(response);
-  EXPECT_EQ(response->quantity(), qty);
-  EXPECT_EQ(q.mass_frac(922350000), product_assay);
-  EXPECT_EQ(q.mass_frac(922380000), 1 - product_assay);
-
-  // test too much natu request
-  DoAddMat(GetMat(natu_req - 1));
-  EXPECT_THROW(response = DoEnrich(target, qty), cyclus::Error);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(EnrichmentTest, Response) {
-  // this test asks the facility to respond to multiple requests for enriched
-  // uranium. two requests are provided, whose total equals the swu capacity of
-  // the facility while not exceeding its inventory capacity (that's taken care
-  // of in the Enrich tests).
-  //
-  // note that response quantity and quality need not be tested, because they
-  // are covered by the Enrich and RequestEnrich tests
-  using cyclus::Bid;
-  using cyclus::CompMap;
-  using cyclus::Material;
-  using cyclus::Request;
-  using cyclus::Trade;
-  using cyclus::toolkit::Assays; 
-  using cyclus::toolkit::FeedQty; 
-  using cyclus::toolkit::SwuRequired;
-  using cyclus::toolkit::UraniumAssay;
-
-  // problem set up
-  std::vector< cyclus::Trade<cyclus::Material> > trades;
-  std::vector<std::pair<cyclus::Trade<cyclus::Material>,
-                        cyclus::Material::Ptr> > responses;
-
-  double qty = 5;  // kg
-  double trade_qty = qty / 3;
-  double product_assay = 0.05;  // of 5 w/o enriched U
-
-  cyclus::CompMap v;
-  v[922350000] = product_assay;
-  v[922380000] = 1 - product_assay;
-  // target qty need not be = to request qty
-  Material::Ptr target = cyclus::Material::CreateUntracked(
-      qty + 10, cyclus::Composition::CreateFromMass(v));
-
-  Assays assays(feed_assay, UraniumAssay(target), tails_assay);
-  double swu_req = SwuRequired(qty, assays);
-  double natu_req = FeedQty(qty, assays);
-  
-  src_facility->SetMaxInventorySize(natu_req * 4);  // not capacitated by nat
-  src_facility->SwuCapacity(swu_req);  // swu capacitated
-  
-  src_facility->GetMatlTrades(trades, responses);
-  
-  // set up state
-  DoAddMat(GetMat(natu_req * 2));
-
-  src_facility->GetMatlTrades(trades, responses);
-
-  Request<Material>* req =
-      Request<Material>::Create(target, trader, product_commod);
-  Bid<Material>* bid = Bid<Material>::Create(req, target, src_facility);
-  Trade<Material> trade(req, bid, trade_qty);
-  trades.push_back(trade);
-
-  // 2 trades, SWU = SWU cap
-    ASSERT_GT(src_facility->SwuCapacity() - 2 * swu_req / 3,
-              -1 * cyclus::eps());
-  trades.push_back(trade);
-  responses.clear();
-  EXPECT_NO_THROW(src_facility->GetMatlTrades(trades, responses));
-  EXPECT_EQ(responses.size(), 2);
-}
-  
-}  // namespace cycamore
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Agent* EnrichmentConstructor(cyclus::Context* ctx) {
-  return new cycamore::Enrichment(ctx);
-}
-
-// required to get functionality in cyclus agent unit tests library
-#ifndef CYCLUS_AGENT_TESTS_CONNECTED
-int ConnectAgentTests();
-static int cyclus_agent_tests_connected = ConnectAgentTests();
-#define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
-#endif  // CYCLUS_AGENT_TESTS_CONNECTED
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-INSTANTIATE_TEST_CASE_P(EnrichmentFac, FacilityTests,
-                        Values(&EnrichmentConstructor));
-INSTANTIATE_TEST_CASE_P(EnrichmentFac, AgentTests,
-                        Values(&EnrichmentConstructor));
+ 
+} // namespace randomenrichtests
+} // namespace mbmore
