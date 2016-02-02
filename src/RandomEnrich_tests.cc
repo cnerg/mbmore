2319747cb82be1a61a31d8b4f6378cf2909fd265
Meghan McGarry <mbmcgarry@wisc.edu>
2016-02-02 16:29:26 -0600

    added state vars n_swipes, fals_pos, false_neg and started unit tests for them

--- src/RandomEnrich_tests.cc
+++ src/RandomEnrich_tests.cc
@@ -533,6 +533,64 @@ TEST(RandomEnrichTests, TestTailsAssay) {
 
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST(RandomEnrichTests, TestInspections) {
+  // Inspections should occur with an average frequency, and swipe rate
+  // should have the expected # of HEU detections given false positive
+  // and false negative rates
+  // TODO: TEST FALSE POSITIVE RATE
+  
+  std::string config = 
+    "   <feed_commod>natu</feed_commod> "
+    "   <feed_recipe>natu1</feed_recipe> "
+    "   <product_commod>enr_u</product_commod> "
+    "   <tails_commod>tails</tails_commod> "
+    "   <tails_assay>0.002</tails_assay> "
+    "   <inspect_freq>2</inspect_freq> "
+    "   <n_swipes>10</n_swipes> "
+    "   <false_pos>0</false_pos> "
+    "   <false_neg>0.5</false_neg> ";
+
+  // time 1-source to EF, 2-Enrich to sink
+  int simdur = 100;
+  cyclus::MockSim sim(cyclus::AgentSpec
+		      (":mbmore:RandomEnrich"), config, simdur);
+  sim.AddRecipe("natu1", c_natu1());
+  sim.AddRecipe("leu", c_leu());
+  sim.AddRecipe("heu", c_heu());
+  
+  sim.AddSource("natu")
+    .recipe("natu1")
+    .Finalize();
+  sim.AddSink("enr_u")
+    .recipe("leu")
+    .capacity(1.0)
+    .Finalize();
+   sim.AddSink("enr_heu")
+    .recipe("heu")
+    .capacity(1.0)
+    .Finalize();
+  
+  int id = sim.Run();
+  std::vector<Cond> conds;
+  conds.push_back(Cond("Time", "<", simdur));
+  QueryResult qr = sim.db().Query("Inspections", &conds);
+  int n_inspect = qr.rows.size();
+
+  double eps = 0.05;
+  double inspect_ratio = double(n_inspect)/double(simdur);
+  EXPECT_NEAR(inspect_ratio, 0.5, eps) <<
+    "expected 50 inspections, got " << n_inspect;
+
+  double swipe_tot = 0;
+  for (int it=0; it < n_inspect; it++){
+    swipe_tot += qr.GetVal<double>("PosSwipeFrac", it);
+  }
+  double pos_rate = swipe_tot/double(n_inspect);
+  EXPECT_NEAR(pos_rate, 0.5, eps);
+
+}
+
  
 } // namespace randomenrichtests
 } // namespace mbmore
