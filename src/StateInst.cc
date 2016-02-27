477378f7bfd8223a24d8765dc46376fe11b1db98
Meghan McGarry <mcgarry.fc@gmail.com>
2016-02-27 11:14:05 -0600

    initial filecreation for StateInst (to model multilateral study)

---
+++ src/StateInst.cc
@@ -0,0 +1,70 @@
+// Implements the StateInst class
+#include "StateInst.h"
+
+namespace mbmore {
+
+StateInst::StateInst(cyclus::Context* ctx) : cyclus::Institution(ctx) {}
+
+StateInst::~StateInst() {}
+
+void StateInst::Build(cyclus::Agent* parent) {
+  cyclus::Institution::Build(parent);
+  BuildSched::iterator it;
+  std::set<std::string> protos;
+  for (int i = 0; i < prototypes.size(); i++) {
+    std::string proto = prototypes[i];
+
+    std::stringstream ss;
+    ss << proto;
+
+    if (lifetimes.size() == prototypes.size()) {
+      cyclus::Agent* a = context()->CreateAgent<Agent>(proto);
+      if (a->lifetime() != lifetimes[i]) {
+        a->lifetime(lifetimes[i]);
+
+        if (lifetimes[i] == -1) {
+          ss << "_life_forever";
+        } else {
+          ss << "_life_" << lifetimes[i];
+        }
+        proto = ss.str();
+        if (protos.count(proto) == 0) {
+          protos.insert(proto);
+          context()->AddPrototype(proto, a);
+        }
+      }
+    }
+
+    int t = build_times[i];
+    for (int j = 0; j < n_build[i]; j++) {
+      context()->SchedBuild(this, proto, t);
+    }
+  }
+}
+
+void StateInst::EnterNotify() {
+  cyclus::Institution::EnterNotify();
+  int n = prototypes.size();
+  if (build_times.size() != n) {
+    std::stringstream ss;
+    ss << "prototype '" << prototype() << "' has " << build_times.size()
+       << " build_times vals, expected " << n;
+    throw cyclus::ValueError(ss.str());
+  } else if (n_build.size() != n) {
+    std::stringstream ss;
+    ss << "prototype '" << prototype() << "' has " << n_build.size()
+       << " n_build vals, expected " << n;
+    throw cyclus::ValueError(ss.str());
+  } else if (lifetimes.size() > 0 && lifetimes.size() != n) {
+    std::stringstream ss;
+    ss << "prototype '" << prototype() << "' has " << lifetimes.size()
+       << " lifetimes vals, expected " << n;
+    throw cyclus::ValueError(ss.str());
+  }
+}
+
+extern "C" cyclus::Agent* ConstructStateInst(cyclus::Context* ctx) {
+  return new StateInst(ctx);
+}
+
+}  // namespace mbmore
---
+++ src/StateInst.h
@@ -0,0 +1,85 @@
+#ifndef MBMORE_SRC_STATE_INST_H_
+#define MBMORE_SRC_STATE_INST_H_
+
+#include <utility>
+#include <set>
+#include <map>
+
+#include "cyclus.h"
+
+namespace mbmore {
+
+typedef std::map<int, std::vector<std::string> > BuildSched;
+
+// Builds and manages agents (facilities) according to a manually specified
+// deployment schedule. Deployed agents are automatically decommissioned at
+// the end of their lifetime.  The user specifies a list of prototypes for
+// each and corresponding build times, number to build, and (optionally)
+// lifetimes.  The same prototype can be specified multiple times with any
+// combination of the same or different build times, build number, and
+// lifetimes.
+class StateInst : public cyclus::Institution {
+  #pragma cyclus note { \
+    "doc": \
+      "Builds and manages agents (facilities) according to a manually" \
+      " specified deployment schedule. Deployed agents are automatically" \
+      " decommissioned at the end of their lifetime.  The user specifies a" \
+      " list of prototypes for" \
+      " each and corresponding build times, number to build, and (optionally)" \
+      " lifetimes.  The same prototype can be specified multiple times with" \
+      " any combination of the same or different build times, build number," \
+      " and lifetimes. " \
+  }
+ public:
+  StateInst(cyclus::Context* ctx);
+
+  virtual ~StateInst();
+
+  #pragma cyclus
+
+  virtual void Build(cyclus::Agent* parent);
+
+  virtual void EnterNotify();
+
+ protected:
+  #pragma cyclus var { \
+    "doc": "Ordered list of prototypes to build.", \
+    "uitype": ("oneormore", "prototype"), \
+    "uilabel": "Prototypes to deploy", \
+  }
+  std::vector<std::string> prototypes;
+
+  #pragma cyclus var { \
+    "doc": "Time step on which to deploy agents given in prototype list " \
+           "(same order).",						\
+    "uilabel": "Deployment times",					\
+  }
+  std::vector<int> build_times;
+
+  #pragma cyclus var { \
+    "doc": "Number of each prototype given in prototype list that should be " \
+           "deployed (same order).", \
+    "uilabel": "Number to deploy", \
+  }
+  std::vector<int> n_build;
+
+
+#pragma cyclus var {							\
+    "doc": "Lifetimes for each prototype in prototype list (same order)." \
+           " These lifetimes override the lifetimes in the original prototype" \
+           " definition." \
+           " If unspecified, lifetimes from the original prototype definitions"\
+           " are used." \
+           " Although a new prototype is created in the Prototypes table for" \
+           " each lifetime with the suffix '_life_[lifetime]'," \
+           " all deployed agents themselves will have the same original" \
+           " prototype name (and so will the Agents tables).", \
+    "default": [], \
+    "uilabel": "Lifetimes" \
+  }
+  std::vector<int> lifetimes;
+};
+
+}  // namespace mbmore
+
+#endif  // MBMORE_SRC_STATE_INST_H_
