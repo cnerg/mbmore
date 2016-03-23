8591aa68be146d0701ad3139452baae6b5b39625
Meghan McGarry <mcgarry.fc@gmail.com>
2016-03-23 16:08:13 -0500

    state I left off in and forget why.

--- src/StateInst.cc
+++ src/StateInst.cc
@@ -6,63 +6,50 @@ namespace mbmore {
 StateInst::StateInst(cyclus::Context* ctx) : cyclus::Institution(ctx) {}
 
 StateInst::~StateInst() {}
+  /*
+    change build time to be based on logic of a PE outcome variable (from Tick?)
+   */
 
+  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void StateInst::Build(cyclus::Agent* parent) {
   cyclus::Institution::Build(parent);
-  BuildSched::iterator it;
-  std::set<std::string> protos;
-  for (int i = 0; i < prototypes.size(); i++) {
-    std::string proto = prototypes[i];
 
-    std::stringstream ss;
-    ss << proto;
+  std::string proto = prototype;
+  
+  cyclus::Agent* a = context()->CreateAgent<Agent>(proto);
+  a->lifetime(-1);
+  
+  context()->AddPrototype(proto, a);
 
-    if (lifetimes.size() == prototypes.size()) {
-      cyclus::Agent* a = context()->CreateAgent<Agent>(proto);
-      if (a->lifetime() != lifetimes[i]) {
-        a->lifetime(lifetimes[i]);
+  context()->SchedBuild(this, proto, build_time);
 
-        if (lifetimes[i] == -1) {
-          ss << "_life_forever";
-        } else {
-          ss << "_life_" << lifetimes[i];
-        }
-        proto = ss.str();
-        if (protos.count(proto) == 0) {
-          protos.insert(proto);
-          context()->AddPrototype(proto, a);
-        }
-      }
-    }
-
-    int t = build_times[i];
-    for (int j = 0; j < n_build[i]; j++) {
-      context()->SchedBuild(this, proto, t);
-    }
-  }
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void StateInst::EnterNotify() {
   cyclus::Institution::EnterNotify();
-  int n = prototypes.size();
-  if (build_times.size() != n) {
-    std::stringstream ss;
-    ss << "prototype '" << prototype() << "' has " << build_times.size()
-       << " build_times vals, expected " << n;
-    throw cyclus::ValueError(ss.str());
-  } else if (n_build.size() != n) {
-    std::stringstream ss;
-    ss << "prototype '" << prototype() << "' has " << n_build.size()
-       << " n_build vals, expected " << n;
-    throw cyclus::ValueError(ss.str());
-  } else if (lifetimes.size() > 0 && lifetimes.size() != n) {
-    std::stringstream ss;
-    ss << "prototype '" << prototype() << "' has " << lifetimes.size()
-       << " lifetimes vals, expected " << n;
-    throw cyclus::ValueError(ss.str());
-  }
+  std::stringstream ss;
+  ss << "prototype '" << prototype << "is deployed at " << context()->time();
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::Tock() {
+  LOG(cyclus::LEV_INFO3, "StateInst") << prototype << " is tocking {";
+
+  //  if (deploy == 1) {
+  LOG(cyclus::LEV_INFO4, "StateInst") << "StateInst " << this->id()
+				      << " is deploying a HEUSink at:" 
+				      << context()->time() << ".";
+  //  }
+  LOG(cyclus::LEV_INFO3, "StateInst") << "}";
+
+    
+
+    
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
 extern "C" cyclus::Agent* ConstructStateInst(cyclus::Context* ctx) {
   return new StateInst(ctx);
 }
