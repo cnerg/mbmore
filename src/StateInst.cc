01955b2926391afa9ee4fb8d618d257e2c68b600
Meghan McGarry <mbmcgarry@wisc.edu>
2016-03-28 17:48:43 -0500

    now compiling, But somehow registering secret archetypes before it should happen

--- src/StateInst.cc
+++ src/StateInst.cc
@@ -3,53 +3,140 @@
 
 namespace mbmore {
 
-StateInst::StateInst(cyclus::Context* ctx) : cyclus::Institution(ctx) {}
-
-StateInst::~StateInst() {}
-  /*
-    change build time to be based on logic of a PE outcome variable (from Tick?)
-   */
-
-  
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void StateInst::Build(cyclus::Agent* parent) {
-  cyclus::Institution::Build(parent);
-
-  std::string proto = prototype;
-  
-  cyclus::Agent* a = context()->CreateAgent<Agent>(proto);
-  a->lifetime(-1);
-  
-  context()->AddPrototype(proto, a);
-
-  context()->SchedBuild(this, proto, build_time);
-
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+StateInst::StateInst(cyclus::Context* ctx) : cyclus::Institution(ctx) {
+  cyclus::Warn<cyclus::EXPERIMENTAL_WARNING>("the StateInst agent is experimental.");
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+StateInst::~StateInst() {}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::BuildNotify(Agent* a) {
+  Register_(a);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::DecomNotify(Agent* a) {
+  Unregister_(a);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void StateInst::EnterNotify() {
   cyclus::Institution::EnterNotify();
-  std::stringstream ss;
-  ss << "prototype '" << prototype << "is deployed at " << context()->time();
+  /*
+  std::set<cyclus::Agent*>::iterator sit;
+  for (sit = cyclus::Agent::children().begin();
+       sit != cyclus::Agent::children().end();
+       ++sit) {
+    Agent* a = *sit;
+    Register_(a);
+  }
+
+  */
+
+  using cyclus::toolkit::CommodityProducer;
+  std::vector<std::string>::iterator vit;
+  for (vit = declared_protos.begin(); vit != declared_protos.end(); ++vit) {
+    Agent* a = context()->CreateAgent<Agent>(*vit);
+    Register_(a);
+
+    CommodityProducer* cp_cast = dynamic_cast<CommodityProducer*>(a);
+    if (cp_cast != NULL) {
+      LOG(cyclus::LEV_INFO3, "mani") << "Registering declared_proto "
+                                     << a->prototype() << a->id()
+                                     << " with the Builder interface.";
+      Builder::Register(cp_cast);
+    }
+  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::Register_(Agent* a) {
+  using cyclus::toolkit::CommodityProducer;
+  using cyclus::toolkit::CommodityProducerManager;
+
+  CommodityProducer* cp_cast = dynamic_cast<CommodityProducer*>(a);
+  //  if (cp_cast != NULL) {
+    LOG(cyclus::LEV_INFO3, "mani") << "Registering agent "
+                                   << a->prototype() << a->id()
+                                   << " as a commodity producer.";
+    CommodityProducerManager::Register(cp_cast);
+    //  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::Unregister_(Agent* a) {
+  using cyclus::toolkit::CommodityProducer;
+  using cyclus::toolkit::CommodityProducerManager;
+
+  CommodityProducer* cp_cast = dynamic_cast<CommodityProducer*>(a);
+  if (cp_cast != NULL)
+    CommodityProducerManager::Unregister(cp_cast);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void StateInst::Tock() {
-  LOG(cyclus::LEV_INFO3, "StateInst") << prototype << " is tocking {";
+  //  LOG(cyclus::LEV_INFO3, "StateInst") << prototype << " is tocking {";
 
-  //  if (deploy == 1) {
-  LOG(cyclus::LEV_INFO4, "StateInst") << "StateInst " << this->id()
-				      << " is deploying a HEUSink at:" 
-				      << context()->time() << ".";
-  //  }
+  
+  if (context()->time() == 4) {
+    LOG(cyclus::LEV_INFO4, "StateInst") << "StateInst " << this->id()
+					<< " is deploying a HEUSink at:" 
+					<< context()->time() << ".";
+    DeploySecret();
+    
+  }
   LOG(cyclus::LEV_INFO3, "StateInst") << "}";
-
     
 
     
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Deploys secret prototypes when called by Tock
+void StateInst::DeploySecret() {
+  //void StateInst::DeploySecret(cyclus::Agent* parent) {
+  //  cyclus::Institution::Build(parent);
+  BuildSched::iterator it;
+  std::cout << "SECRET DEPLOY!!!! at " << context()->time() << std::endl;
+  
+  for (int i = 0; i < secret_protos.size(); i++) {
+    std::string s_proto = secret_protos[i];
+    
+    std::stringstream ss;
+    ss << s_proto;
+    
+    cyclus::Agent* a = context()->CreateAgent<Agent>(s_proto);
+    s_proto = ss.str();
+    context()->AddPrototype(s_proto, a);
+    context()->SchedBuild(this, s_proto);  //builds on next timestep
+  }
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StateInst::WriteProducerInformation(
+  cyclus::toolkit::CommodityProducer* producer) {
+  using std::set;
+  set<cyclus::toolkit::Commodity,
+      cyclus::toolkit::CommodityCompare> commodities =
+          producer->ProducedCommodities();
+  set<cyclus::toolkit::Commodity, cyclus::toolkit::CommodityCompare>::
+      iterator it;
+
+  LOG(cyclus::LEV_DEBUG3, "maninst") << " Clone produces " << commodities.size()
+                                     << " commodities.";
+  for (it = commodities.begin(); it != commodities.end(); it++) {
+    LOG(cyclus::LEV_DEBUG3, "maninst") << " Commodity produced: " << it->name();
+    LOG(cyclus::LEV_DEBUG3, "maninst") << "           capacity: " <<
+                                       producer->Capacity(*it);
+    LOG(cyclus::LEV_DEBUG3, "maninst") << "               cost: " <<
+                                       producer->Cost(*it);
+  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 extern "C" cyclus::Agent* ConstructStateInst(cyclus::Context* ctx) {
   return new StateInst(ctx);
 }
--- src/StateInst.h
+++ src/StateInst.h
@@ -1,66 +1,82 @@
 #ifndef MBMORE_SRC_STATE_INST_H_
 #define MBMORE_SRC_STATE_INST_H_
 
-#include <utility>
-#include <set>
-#include <map>
-
 #include "cyclus.h"
 
 namespace mbmore {
 
 typedef std::map<int, std::vector<std::string> > BuildSched;
-
-// Builds and manages agents (facilities) according to a manually specified
-// deployment schedule. Deployed agents are automatically decommissioned at
-// the end of their lifetime.  The user specifies a list of prototypes for
-// each and corresponding build times, number to build, and (optionally)
-// lifetimes.  The same prototype can be specified multiple times with any
-// combination of the same or different build times, build number, and
-// lifetimes.
-class StateInst : public cyclus::Institution {
-  #pragma cyclus note { \
-    "doc": \
-      "Builds and manages agents (facilities) according to a manually" \
-      " specified deployment schedule. Deployed agents are automatically" \
-      " decommissioned at the end of their lifetime.  The user specifies a" \
-      " list of prototypes for" \
-      " each and corresponding build times, number to build, and (optionally)" \
-      " lifetimes.  The same prototype can be specified multiple times with" \
-      " any combination of the same or different build times, build number," \
-      " and lifetimes. " \
-  }
-
- private:
-
-  std::string prototype = "Secret_Sink";
-  int build_time = 10;
-
   
+/// @class StateInst
+/// @section introduction Introduction
+/// @section detailedBehavior Detailed Behavior
+/// @warning The StateInst is experimental
+class StateInst
+    : public cyclus::Institution,
+      public cyclus::toolkit::CommodityProducerManager,
+      public cyclus::toolkit::Builder {
  public:
+  /// Default constructor
   StateInst(cyclus::Context* ctx);
 
+  /// Default destructor
   virtual ~StateInst();
 
   #pragma cyclus
 
-  virtual void Build(cyclus::Agent* parent);
+  #pragma cyclus note {"doc": "An institution that owns and operates a " \
+                              "manually entered list of facilities in " \
+                              "the input file"}
 
+  /// enter the simulation and register any children present
   virtual void EnterNotify();
+
+  /// register a new child
+  virtual void BuildNotify(Agent* m);
+
+  /// unregister a child
+  virtual void DecomNotify(Agent* m);
+
+  /// deploy secret child facilities
+  //  virtual void DeploySecret(cyclus::Agent* parent);
+  virtual void DeploySecret();
 
   virtual void Tock();
 
+  
+  /// write information about a commodity producer to a stream
+  /// @param producer the producer
+  void WriteProducerInformation(cyclus::toolkit::CommodityProducer*
+                                producer);
+
+ private:
+  /// register a child
+  void Register_(cyclus::Agent* agent);
+
+  /// unregister a child
+  void Unregister_(cyclus::Agent* agent);
+
+  #pragma cyclus var { \
+    "tooltip": "Declared facility prototypes (at start of sim)",         \
+    "uilabel": "Producer Prototype List",                               \
+    "uitype": ["oneormore", "prototype"],                                    \
+    "doc": "A set of facility prototypes that this institution can build. " \
+    "All prototypes in this list must be based on an archetype that "   \
+    "implements the cyclus::toolkit::CommodityProducer interface",      \
+    }
+  std::vector<std::string> declared_protos;
+
  protected:
-  #pragma cyclus var {"default": 0, "tooltip": "pursuit param" ,	\
-    "doc": "input for pursuit eqn"}
-  
-  double pursuit_param;
+  #pragma cyclus var { \
+    "uilabel": "Secret prototypes to deploy", \
+    "uitype": ("oneormore", "prototype"), \
+    "doc": "Ordered list of secret prototypes to build."   \
+           "These are built only if the Pursuit Equation == 1",   \
+  }
+  std::vector<std::string> secret_protos;
 
 
-  
-
-};
-
+  }; // Toolkit::Builder
 }  // namespace mbmore
 
 #endif  // MBMORE_SRC_STATE_INST_H_
