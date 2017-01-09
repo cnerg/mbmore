cec295860ebe850c063456e031f8b03ead376b8c
Meghan McGarry <mbmcgarry@wisc.edu>
2017-01-09 16:53:54 -0600

    cleaning up docs

--- src/enrich_functions.cc
+++ src/enrich_functions.cc
@@ -13,18 +13,15 @@ namespace mbmore {
  
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // TODO:
-// CONVERT N stages, N machines to integer-limited
-// annotate assumed units, Glaser paper reference, Z_P (What is it?)
-// Math Functions to Check: pi (M_PI??), exp??
+// annotate assumed units, Glaser paper reference
 
   double CalcDelU(double v_a, double height, double diameter,  double feed,
 		  double temp, double cut, double eff, double M, double dM,
 		  double x, double flow_internal) {
 
-    
+    // Inputs that are effectively constants:
     // flow_internal = 2-4, x = pressure ratio, M = 0.352 kg/mol of UF6,
     // dM = 0.003 kg/mol diff between 235 and 238
-    
 
     double a = diameter / 2.0;  // outer radius
 
@@ -33,7 +30,7 @@ namespace mbmore {
     double r_2 = 0.99 * a; 
 
     double r_12 = std::sqrt(1.0 - (2.0 * gas_const * temp*(log(x)) / M /
-				   (pow(v_a,2)))); 
+				   (pow(v_a, 2)))); 
     double r_1 = r_2 * r_12; // withdrawl radius for ligher isotope
 
     // Glaser eqn 12
@@ -51,8 +48,8 @@ namespace mbmore {
     double C_therm = CalcCTherm(v_a, temp, dM);
 
     // defining terms in the Ratz equation
-    double r12_sq = pow(r_12,2);
-    double C_scale = (pow((r_2 / a), 4)) * (pow((1 - r12_sq),2));
+    double r12_sq = pow(r_12, 2);
+    double C_scale = (pow((r_2 / a), 4)) * (pow((1 - r12_sq), 2));
     double bracket1 = (1 + flow_internal) / cut;
     double exp1 = exp(-1.0 * A_p * Z_p);
     double bracket2 = flow_internal/(1 - cut);
@@ -60,7 +57,7 @@ namespace mbmore {
 
     // Glaser eqn 10 (Ratz Equation)
     double major_term = 0.5 * cut * (1.0 - cut) * (pow(C_therm, 2)) * C_scale *
-      pow(((bracket1 * (1 - exp1)) + (bracket2 * (1 - exp2))),2); // kg/s    
+      pow(((bracket1 * (1 - exp1)) + (bracket2 * (1 - exp2))), 2); // kg/s    
     double del_U = feed * major_term * eff; // kg/s
     
     return del_U;
@@ -75,8 +72,6 @@ namespace mbmore {
     return (2.0 * assay - 1.0) * log(assay / (1.0 - assay));
   }
 
-  // Avery p 18
-  // del_U should have units of moles/sec
   double AlphaBySwu(double del_U, double feed, double cut, double M){
     double alpha = 1 + std::sqrt((2 * (del_U / M) * (1 - cut) / (cut * feed)));
     return alpha;
@@ -88,18 +83,14 @@ namespace mbmore {
     return 1.0 / (ratio + 1.0);
   }
 
-  // Avery p 59 (per machine)
   double NWasteByAlpha(double alpha, double feed_assay){
     double A = (feed_assay / (1 - feed_assay)) / alpha;
     return A / (1 + A);
   }
 
-  // Avery ???
-  // TODO: CONVERT THIS TO INTEGER NUMBER!!
   std::pair<double, double> StagesPerCascade(double alpha, double feed_assay,
 					     double product_assay,
 					     double waste_assay){
-
     using std::pair;
 
     double epsilon = alpha - 1.0;
@@ -156,7 +147,6 @@ namespace mbmore {
   double MachinesPerCascade(double del_U_machine, double product_assay,
 			    double waste_assay, double feed_flow,
 			    double product_flow){
-    // Avery p 62
     double U_cascade = DeltaUCascade(product_assay, waste_assay, feed_flow,
 				     product_flow);
     return U_cascade / del_U_machine;
