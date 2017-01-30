c7b1fa5e3d6b0346602e4eef4ea1e41a6b194fcf
Meghan McGarry <mcgarry.fc@gmail.com>
2017-01-30 10:58:08 -0600

    working on build phase

--- src/CascadeEnrich.cc
+++ src/CascadeEnrich.cc
@@ -42,6 +42,21 @@ void CascadeEnrich::Build(cyclus::Agent* parent) {
         this, initial_feed, context()->GetRecipe(feed_recipe)));
   }
 
+  // Calculate ideal machine performance
+  design_delU = CalcDelU(v_a, height, diameter, machine_feed, temp,
+				cut, eff, M, dM, x, flow_internal);
+  design_alpha = AlphaBySwu(design_delU, machine_feed, cut, M);
+
+  // Design ideal cascade based on target feed flow and product assay
+  n_enrich_stages = CascadeEnrich::NEnrichStages(design_alpha,
+							design_delU,
+							cascade_feed,
+							design_feed_assay);
+  n_strip_stages = CascadeEnrich::NStripStages(design_alpha,
+						   design_delU,
+						   cascade_feed,
+						   design_feed_assay);
+
   LOG(cyclus::LEV_DEBUG2, "EnrFac") << "CascadeEnrich "
 				    << " entering the simuluation: ";
   LOG(cyclus::LEV_DEBUG2, "EnrFac") << str();
@@ -50,12 +65,7 @@ void CascadeEnrich::Build(cyclus::Agent* parent) {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void CascadeEnrich::Tick() {
 
-  /*
-  double delU = CalcDelU(v_a, height, diameter, feed, temp, cut, eff,
-			 M, dM, x, flow_internal);
 
-  std::cout << "DelU is " << delU << std::endl;
-  */
  }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -66,8 +76,71 @@ void CascadeEnrich::Tock() {
 
 
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ int CascadeEnrich::NEnrichStages(double alpha, double delU, double feed,
+				     double feed_assay){
 
+   double stage_feed_assay = feed_assay;
+   double stage_feed = feed;
+   bool integer_stages = true;
+   int n_stage_enr = 0;
+   double product_assay, stage_product;
+   
+   while (integer_stages == true){
+     double n_mach = MachinesPerStage(alpha, delU, stage_feed);
+     if (((int) n_mach) <= 1){ integer_stages = false; }
+     product_assay = ProductAssayByAlpha(alpha, stage_feed_assay);
+     stage_product = ProductPerEnrStage(alpha, stage_feed_assay, product_assay,
+					stage_feed);
+     stage_feed_assay = product_assay;
+     stage_feed = stage_product; 
+     n_stage_enr += 1; 
+     if (n_stage_enr == 15){ integer_stage = false;} // just a catch until I  make sure while loop is working
 
+   }
+
+   return n_stage_enr;
+
+ }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ int CascadeEnrich::NStripStages(double alpha, double delU, double enr_feed,
+				     double feed_assay){
+
+   bool integer_stages = true;
+   int n_stage_strip = 0;
+   double waste_assay, stage_waste;
+
+   // the waste from first enrichment stage is the feed for the stripping stages
+   double enr_product_assay = ProductAssayByAlpha(alpha, feed_assay);
+   double enr_product = ProductPerEnrStage(alpha, feed_assay, enr_product_assay,
+					   enr_feed);
+
+   double enr_waste = enr_feed - enr_product;
+   double enr_waste_assay = (enr_feed*feed_assay -
+			     enr_product*enr_product_assay) / enr_waste;
+   
+   double stage_feed_assay = enr_waste_assay;
+   double stage_feed = enr_waste;
+   
+   while (integer_stages == true){
+     double n_mach = MachinesPerStage(alpha, delU, stage_feed);
+     if (((int) n_mach) <= 1){ integer_stages = false; }
+     waste_assay = WasteAssayByAlpha(alpha, stage_feed_assay);
+     stage_waste = WastePerStripStage(alpha, stage_feed_assay, waste_assay,
+					stage_feed);
+     stage_feed_assay = waste_assay;
+     stage_feed = stage_waste; 
+     n_stage_strip += 1;
+     
+     if (n_stage_strip == 15){ integer_stage = false;} // just a cath until I  make sure while loop is working
+
+   }
+
+   return n_stage_strip;
+   
+
+ }
+  
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 extern "C" cyclus::Agent* ConstructCascadeEnrich(cyclus::Context* ctx) {
   return new CascadeEnrich(ctx);
