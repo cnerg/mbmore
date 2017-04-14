8413ba705655bef1acbaca5329e08b437f95d5fc
Meghan McGarry <mcgarry.fc@gmail.com>
2017-04-14 17:46:16 -0500

    linear solver works if real arrays are used, but not with pointers

--- src/enrich_functions.h
+++ src/enrich_functions.h
@@ -99,7 +99,8 @@ extern "C" {
   // Solves system of linear eqns to determine steady state flow rates
   // in each stage of cascade
   //  std::vector<double> CalcFeedFlows(std::pair<double, double> n_st,
-  double** CalcFeedFlows(std::pair<double, double> n_st,
+  //  double** CalcFeedFlows(std::pair<double, double> n_st,
+  void CalcFeedFlows(std::pair<double, double> n_st,
 			 double cascade_feed, double cut);
 
   
