d90a8fd1c80adfc816851c0ed3798493fa10898c
Mouginot B <mouginot.baptiste@gmail.com>
2017-11-27 16:32:31 -0600

    first compilation

--- src/CascadeConfig.cc
+++ src/CascadeConfig.cc
@@ -4,7 +4,9 @@
 #include <algorithm>
 #include <boost/lexical_cast.hpp>
 #include <cmath>
+#include <iostream>
 #include <limits>
+#include <map>
 #include <sstream>
 #include <vector>
 
@@ -12,8 +14,8 @@ namespace mbmore {
 
 CascadeConfig::CascadeConfig() {}
 
-CascadeConfig(double f_assay, double p_assay, double t_assay,
-              double max_feed_flow, int max_centrifuge) {
+CascadeConfig::CascadeConfig(double f_assay, double p_assay, double t_assay,
+                             double max_feed_flow, int max_centrifuge) {
   double feed_assay = f_assay;
   double product_assay = p_assay;
   double tail_assay = t_assay;
@@ -22,7 +24,7 @@ CascadeConfig(double f_assay, double p_assay, double t_assay,
   int n_centrifuge = max_centrifuge;
 
   BuildIdealCascade(feed_assay, product_assay, tail_assay);
-  DesignCascade(max_feed_flow, max_centrifuge)
+  DesignCascade(max_feed_flow, max_centrifuge);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -33,7 +35,7 @@ CascadeConfig(double f_assay, double p_assay, double t_assay,
 // feed stage (F_0) stages start with last strip stage [-2, -1, 0, 1, 2]
 //  http://www.physics.utah.edu/~detar/phys6720/handouts/lapack.html
 //
-void CasacdeConfig::CalcFeedFlows() {
+void CascadeConfig::CalcFeedFlows() {
   // This is the Max # of stages in  It cannot be passed in due to
   // how memory is allocated and so must be hardcoded. It's been chosen
   // to be much larger than it should ever need to be
@@ -48,8 +50,8 @@ void CasacdeConfig::CalcFeedFlows() {
     exit(1);
   }
   double feed_assay = stgs_config[0].feed_assay;
-  double product_assay = stgs_config[enrich_stgs - 1].product_assay;
-  double tail_assay = stgs_config[-stripping_stgs].tail_assay;
+  double product_assay = stgs_config[n_enrich - 1].product_assay;
+  double tail_assay = stgs_config[-n_strip].tail_assay;
 
   double product_flow =
       feed_flow * (feed_assay - tail_assay) / (product_assay - tail_assay);
@@ -117,25 +119,29 @@ void CasacdeConfig::CalcFeedFlows() {
 
   for (int i = 0; i < n_stages; i++) {
     int stg_i = i - n_strip;
-    stgs_config[stg_i].flow = flows[0][i];
+    stgs_config[stg_i].feed_flow = flows[0][i];
   }
 }
 
 void CascadeConfig::BuildIdealCascade(double feed_assay, double product_assay,
                                       double waste_assay, double precision) {
-  map<int, StageConfig> ideal_stgs;
+  std::map<int, StageConfig> ideal_stgs;
   int ideal_enrich_stage = 0;
   int ideal_strip_stage = 0;
 
   // Initialisation of Feeding stage (I == 0)
-  StageConfig stg = BuildIdealStg(feed_assay, centrifuge, -1, -1, precision);
+  StageConfig stg;
+  stg.alpha = -1;
+  stg.DU = -1;
+  stg.centrifuge = centrifuge;
+  stg.BuildIdealStg(feed_assay, precision);
   int stg_i = 0;
   ideal_stgs[stg_i] = stg;
   double ref_alpha = ideal_stgs[0].alpha;
   double ref_du = ideal_stgs[0].DU;
   // Calculate number of enriching stages
   while (stg.product_assay < product_assay) {
-    stg = BuildIdealStg(stg.product_assay, ref_du, ref_alpha, precision);
+    stg.BuildIdealStg(stg.product_assay, precision);
     stg_i++;
     ideal_stgs[stg_i] = stg;
   }
@@ -145,8 +151,7 @@ void CascadeConfig::BuildIdealCascade(double feed_assay, double product_assay,
   stg = ideal_stgs[stg_i];
   // Calculate number of stripping stages
   while (stg.tail_assay > waste_assay) {
-    stg =
-        BuildIdealStg(stg.tail_assay, centrifuge, ref_du, ref_alpha, precision);
+    stg.BuildIdealStg(stg.tail_assay, precision);
     stg_i--;
     ideal_stgs[stg_i] = stg;
   }
@@ -154,13 +159,42 @@ void CascadeConfig::BuildIdealCascade(double feed_assay, double product_assay,
 
   stgs_config = ideal_stgs;
 }
+void CascadeConfig::CalcStageFeatures() {
+  double machine_tol = 0.01;
+  int n_enrich = n_enrich;
+  int n_strip = n_strip;
+  int n_stages = n_enrich + n_strip;
 
-void DesignCascade(double max_feed, int max_centrifuges) {
+  for (int i = 0; i < n_stages; i++) {
+    int curr_stage = i - n_strip;
+    double stage_feed = stgs_config[curr_stage].feed_flow;
+    double alpha = stgs_config[curr_stage].alpha;
+    double DU = stgs_config[curr_stage].DU;
+    double n_mach_exact = stgs_config[curr_stage].MachinesPerStage();
+    // unless the ideal number of machines is Very close to an integer value,
+    // round up to next integer to preserve steady-state flow balance
+    int n_mach = (int)n_mach_exact;
+    if (std::abs(n_mach_exact - n_mach) > machine_tol) {
+      n_mach = int(n_mach_exact) + 1;
+    }
+    stgs_config[curr_stage].n_machines = n_mach;
+  }
+}
+
+int CascadeConfig::FindTotalMachines() {
+  int machines = 0;
+  for (int i = -n_strip; i < n_enrich; i++) {
+    machines += stgs_config[i].n_machines;
+  }
+  return machines;
+}
+
+void CascadeConfig::DesignCascade(double max_feed, int max_centrifuges) {
   // Determine the ideal steady-state feed flows for this cascade design given
   // the maximum potential design feed rate
   feed_flow = max_feed;
   CalcFeedFlows();
-  CalcStageFeatures(max_feed_cascade);
+  CalcStageFeatures();
 
   // Do design parameters require more centrifuges than what is available?
   int machines_needed = FindTotalMachines();
@@ -175,7 +209,6 @@ void DesignCascade(double max_feed, int max_centrifuges) {
   }
 
   n_machines = machines_needed;
-  return max_feed_cascade;
 }
 
 }  // namespace mbmore
