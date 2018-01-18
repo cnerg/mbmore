0043f2da914dd0583fc5d97cd3c6952215c2a737
Mouginot B <mouginot.baptiste@gmail.com>
2018-01-18 15:15:05 -0600

    adding some comments on centrifue

--- src/CentrifugeConfig.h
+++ src/CentrifugeConfig.h
@@ -14,27 +14,27 @@ class CentrifugeConfig {
   CentrifugeConfig(double v_a, double h, double d, double feed, double T,
                    double eff, double M, double dM, double x, double i_flow);
 
-  double ComputeDeltaU(double cut);
+  double ComputeDeltaU(double cut); // compute the solution of the Raetz equation using all the paramters valus and the provided cut value
 
   // flow_internal = 2-4, x = pressure ratio, M = 0.352 kg/mol of UF6,
   // dM = 0.003 kg/mol diff between 235 and 238
-  double M;
-  double dM;
-  double x;
-  double flow_internal;
+  double dM; // molar mass diff between the 2 components of the gaz default 0.003kg/mol (u235/U238)
+  double M;  // gaz molar mass, default UF6 0.352 kg/mol
+  double x;  // pressure ration -> drive r1/r2 ratio
+  double flow_internal; // countercurrent-to-feed ratios k range between 2 and 4
 
-  double v_a;
-  double height;
-  double diameter;
-  double feed;
-  double temp;
-  double eff;
+  double v_a; // peripheral velocities
+  double height; // centrifugre height
+  double diameter; // centrifuge diameter
+  double feed; // feed flow rate
+  double temp; // average gaz temperature
+  double eff; // efficiency
 
  private:
   double D_rho;      // kg/m/s
   double gas_const;  // J/K/mol
   double M_238;      // kg/mol
-  double secpermonth;
+  double secpermonth; // obvious ?!
 
   double CalcCTherm(double v_a, double temp, double dM);
   double CalcV(double assay);
