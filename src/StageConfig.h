cdf96c9f2db702003ca9db56a58a628d9d854508
Mouginot B <mouginot.baptiste@gmail.com>
2018-01-18 16:45:57 -0600

    adding coments on StageConfig

--- src/StageConfig.h
+++ src/StageConfig.h
@@ -14,36 +14,53 @@ class StageConfig {
   StageConfig() {;}
   StageConfig(double f_assay, double feed_flow, double precision, double cut = -1, double DU = -1, double alpha = -1); 
 
+  // Build a stage assumming alpha = beta (if cut is not defined, compute the cut to make it so)
   void BuildIdealStg(double f_assay, double precision = 1e-8);
+  // Compute the cut to ensure alpha = beta (from dU)
   double CutForIdealStg(double f_assay, double precision = 1e-8);
 
+  // calculate Alpha value using the dU, Cut and the centrifuge feed flow value
   double AlphaByDU();
 
+  // Compute Beta value from alpha value, cut and feed assay
   double BetaByAlphaAndCut();
+  // recompute Cut value assuming Alpha and Beta fixed
   double CutByAlphaBeta();
 
+  // COmpute Product assay from feed assay and alpha
   double ProductAssay();
+  // Compute Waste assy from feed assay and beta
   double TailAssay();
 
+  // Return the minimum number of centrifudes required to meed the feed flow
   double MachinesPerStage();
+  // Compute the Product feed 
   double ProductPerEnrStage();
 
-  // Calculates the V(N_x) term for enrichment eqns where N_x is the assay
-  // of isotope x
-
+  // Configuration of all the centrifuges in the stage
   CentrifugeConfig centrifuge;
+  // Precision used for the cut calculation defautl 1e-8
   double precision;
 
+  // cut value of the stage
   double cut;
+  // dU value of the stage (calculted form the centrifuges config with the cut)
   double DU;
+  // Feed to Product enrichment ratio
   double alpha;
+  // Feed to Tail enrichment ratio
   double beta;
+  // Feed flow (g/s)
   double feed_flow;
   
+  // number of centriges in the stage
   double n_machines;
-
+  
+  // Feed assay
   double feed_assay;
+  // Product assay
   double product_assay;
+  // Tail assay
   double tail_assay;
 };
 
